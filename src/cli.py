65f9b947746978d2efb0291735d85786ff4a0049
tomas polach <tomas@polach.ch>
2024-07-07 17:22:38 +0200

    different param name convention

--- src/cli.py
+++ src/cli.py
@@ -10,31 +10,27 @@ from src.deploy import Deployment
 def main():
     # just print all the raw args from the cli
     for arg in sys.argv:
-        print(arg)
+        print('ARGGGG:', arg)
 
     parser = argparse.ArgumentParser(description='RuDeploy to Docker Compose to AWS')
 
-    parser.add_argument('--aws_region', type=str, required=True, help='The AWS region', default=os.getenv('INPUT_AWS_REGION'))
+    parser.add_argument('--aws-region', type=str, required=True, help='The AWS region', default=os.getenv('INPUT_AWS_REGION'))
 
-    parser.add_argument('--cf_stack_prefix', type=str, required=False, help='Prefix for the Cloudformation Stack')
+    parser.add_argument('--cf-stack-prefix', type=str, required=False, help='Prefix for the Cloudformation Stack')
     parser.add_argument('--environment', type=str, required=False,
                         help='The environment (will be added as suffix to the stack name)')
 
-    parser.add_argument('--domain', type=str, required=False, help='The domain')
-    parser.add_argument('--domain_role_arn', type=str, required=False, help='The domain role ARN')
-    parser.add_argument('--cert_role_arn', type=str, required=False, help='The certificate role ARN')
+    parser.add_argument('--elb-domain', type=str, required=False, help='The domain to map to elastic load balancer')
+    parser.add_argument('--elb-domain-role-arn', type=str, required=False, help='The domain role ARN')
 
-    parser.add_argument('--docker_compose_path', type=str, required=False,
+    parser.add_argument('--docker-compose-path', type=str, required=False,
                         help='The docker compose path')
-    parser.add_argument('--aws_compose_path', type=str, required=False,
+    parser.add_argument('--aws-compose-path', type=str, required=False,
                         help='The AWS compose path')
-    # parser.add_argument('--temp_dir', type=str, required=False, help='The temporary directory')
-    # parser.add_argument('--mutable_tags', type=bool, required=False, help='Whether the tags are mutable')
-    # parser.add_argument('--image_uri_format', type=str, required=False, help='The image URI format')
 
     args = parser.parse_args()
-
-    pp(args)
+    # Convert argument names with dashes to underscores
+    args = {k.replace('-', '_'): v for k, v in vars(args).items()}
 
     # Use the provided project_name or default to the repository name
     if args.cf_stack_prefix is None:
