60158f386a2f9e43b5ab8790bf9f8c45374fccfb
tomas polach <tomas@polach.ch>
2024-07-08 06:42:01 +0200

    debug

--- src/cli.py
+++ src/cli.py
@@ -8,6 +8,8 @@ from src.deploy import Deployment
 
 
 def main():
+    print(os.getenv('AWS_ACCESS_KEY_ID'))
+
     # just print all the raw args from the cli
     parser = argparse.ArgumentParser(description='RuDeploy to Docker Compose to AWS')
 
